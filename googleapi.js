61098d40527ff9d83b8c00801d9b859c0d23adc0
aterna01 <ssaniok90@mail.ru>
2018-11-15 12:41:48 +0000

    add icons

--- googleapi.js
+++ googleapi.js
@@ -9,32 +9,38 @@
   xhr.onreadystatechange = function() {
     if (xhr.readyState == 4 && xhr.status == 200) {
       var googleDirection = JSON.parse(xhr.responseText);
-
       var array = googleDirection.journeys[0].legs;
-
       var newArray = [];
+
       for (let i = 0; i < array.length; i++) {
         newArray.push(array[i].instruction.summary);
-        console.log("array check", array[i].instruction.summary);
       }
-      console.log("new array", newArray);
     }
-    document.getElementById("duration").innerHTML =
+    document.getElementById("duration").innerHTM =
       googleDirection.journeys[0].duration + " mins";
-    document.getElementById("direction").innerHTML = newArray;
-    // googleDirection.journeys[0].legs[0].instruction.summary;
-    // console.log("test", googleDirection.journeys[0].legs[0]);
+
     var counter = 0;
     var ul = document.getElementById("legs");
     while (newArray.length > counter) {
       var li = document.createElement("li");
       li.textContent = newArray[counter];
+      if (newArray[counter].includes("Walk")) {
+        var dateSpan = document.createElement("span");
+        dateSpan.innerHTML = '<i class="fas fa-walking"></i>';
+        li.insertBefore(dateSpan, li.firstChild);
+      } else if (newArray[counter].includes("bus")) {
+        var dateSpan = document.createElement("span");
+        dateSpan.innerHTML = '<i class="fas fa-bus"></i>';
+        li.insertBefore(dateSpan, li.firstChild);
+      } else {
+        var dateSpan = document.createElement("span");
+        dateSpan.innerHTML = '<i class="fas fa-train"></i>';
+        li.insertBefore(dateSpan, li.firstChild);
+      }
       ul.appendChild(li);
       counter++;
     }
   };
   xhr.open("GET", url, true);
-  xhr.send(); //included this
+  xhr.send();
 })();
-
-// instruction.summary;
