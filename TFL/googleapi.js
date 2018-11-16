669bf120f77ce880e3fe389fe162a6f1ed171911
aterna01 <ssaniok90@mail.ru>
2018-11-16 00:06:34 +0000

    final

--- TFL/googleapi.js
+++ TFL/googleapi.js
@@ -1,33 +1,46 @@
-// const app_id = "df5c45a2";
-// const app_key = "e753bebcf92ac22a1f8d97c0df442e68";
+//get geolocation of user
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else {
+    alert("Geolocation is not supported by this browser.");
+  }
+}
+
+function showPosition(position) {
+  document.getElementById("invisible").textContent =
+    position.coords.latitude + "," + position.coords.longitude;
+}
+
+getLocation();
 
 function showRoute(event) {
-  // clear the current route
   document.getElementById("legs").textContent = "";
+  var invisible = document.getElementById("invisible").textContent;
+  // console.log(invisible);
   var postCode = event.target.className;
-  console.log(postCode);
+  // console.log(postCode);
   var xhr = new XMLHttpRequest();
   var url =
-    "https://api.tfl.gov.uk/journey/journeyresults/n166uz/to/" +
+    "https://api.tfl.gov.uk/journey/journeyresults/" +
+    invisible +
+    "/to/" +
     postCode +
     "?app_id=df5c45a2&app_key=e753bebcf92ac22a1f8d97c0df442e68";
-  console.log(url);
+  // console.log(url);
   xhr.onreadystatechange = function() {
     if (xhr.readyState == 4 && xhr.status == 200) {
       var googleDirection = JSON.parse(xhr.responseText);
       var array = googleDirection.journeys[0].legs;
-
       document.getElementById("duration").innerHTML =
         googleDirection.journeys[0].duration + " mins";
-
       // direction list
-
       var newArray = [];
-
       for (let i = 0; i < array.length; i++) {
         newArray.push(array[i].instruction.summary);
       }
     }
+
     var counter = 0;
     var ul = document.getElementById("legs");
     while (newArray.length > counter) {
@@ -49,7 +62,6 @@ function showRoute(event) {
       ul.appendChild(li);
       counter++;
     }
-
     // setoff and arrival time
     var start = googleDirection.journeys[0].startDateTime
       .split("T")[1]
@@ -57,15 +69,11 @@ function showRoute(event) {
     var arrival = googleDirection.journeys[0].arrivalDateTime
       .split("T")[1]
       .slice(0, 5);
-    console.log(start);
-    console.log(arrival);
+    // console.log(start);
+    // console.log(arrival);
     document.getElementById("start").innerHTML = start + " " + arrival;
   };
-
   xhr.open("GET", url, true);
   xhr.send();
 }
-
 document.getElementById("events").addEventListener("click", showRoute, false);
-//
-// logic.firstAPICall();
